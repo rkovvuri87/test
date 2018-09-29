3d47a43537431f8c90b38152f810949b6fdbbbd5
ramu kovvuri <ramueduweb@gmail.com>
2018-09-29 12:23:22 +0530

    init project

---
+++ fsapi/src/main/java/com/fs/ramu/fsapi/FsapiApplication.java
@@ -0,0 +1,12 @@
+package com.fs.ramu.fsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsapiApplication.class, args);
+	}
+}
